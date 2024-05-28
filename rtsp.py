cb84911a7bf090d1604ebe6176a584c151debb4c
yungshun317 <seraph317@gmail.com>
2024-05-28 08:50:39 +0800

    [FEAT] Play multiple videos simultaneously

--- rtsp.py
+++ rtsp.py
@@ -1,17 +1,35 @@
 import cv2
 
-rtsp = "rtsp://localhost:8554/stream"
+rtsp_urls = ["rtsp://localhost:8554/stream"] * 8
+window_titles = ["frame_" + str(i) for i in range(8)]
 
-cam_0 = cv2.VideoCapture(rtsp)
+cam = [cv2.VideoCapture(i) for i in rtsp_urls]
+ret = [None] * len(rtsp_urls)
+frames = [None] * len(rtsp_urls)
 
-while cam_0.isOpened():
-    ret_0, frame_0 = cam_0.read()
-    if not ret_0:
-        print("Can't receive frame (stream end?). Exit...")
-        break
-    cv2.imshow("frame_0", frame_0)
-    if cv2.waitKey(1) == ord("q"):
-        break
+# Camera
+try:
+    # while cam_0.isOpened():
+    #     ret_0, frame_0 = cam_0.read()
+    #     if not ret_0:
+    #         print("Can't receive frame (stream end?). Exit...")
+    #         break
+    #     cv2.imshow("frame_0", frame_0)
+    #     if cv2.waitKey(1) == ord("q"):
+    #         break
+    while True:
+        for i, c in enumerate(cam):
+            if c is not None:
+                ret[i], frames[i] = c.read()
 
-cam_0.release()
-cv2.destroyAllWindows()
+        for i, f in enumerate(frames):
+            if ret[i] is True:
+                cv2.imshow(window_titles[i], f)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break            
+except KeyboardInterrupt:
+    pass
+finally:
+    for c in cam:
+        c.release()
+    cv2.destroyAllWindows()
